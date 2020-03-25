dcba830cb65ea71f2d20c305b80465999fe02d96
saiquaan5 <vandung0829@gmail.com>
2020-03-25 15:31:58 +0700

    fist commit

---
+++ project1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+// String style C
+
+char a[] = "jfjjfdsjj";
+char b[] = {'d','d','d'};
+char *c = "fdsfdsfsdf";
+
+
+int main()
+{
+    printf("Hien thi: %s\n",c);
+    return 0;
+}
